0c931f5f829d74b8f168bae6c654da6b56487e55
rahinlaskar8-hub <rahinlaskar8@gmail.com>
2026-09-23 07:40:45 +0530

    Update server launch configuration in app.py

--- app.py
+++ app.py
@@ -27,4 +27,6 @@ interface = gr.Interface(
     description="Upload a chest X-Ray image to detect signs of Pneumonia."
 )
 
-interface.launch()
+import os
+interface.launch(server_name="0.0.0.0",
+                 server_port=int(os.environ.get("PORT",7860)))
